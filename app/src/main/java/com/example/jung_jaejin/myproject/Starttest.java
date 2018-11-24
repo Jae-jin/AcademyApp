b76939a9c758d40ba9f16aaf88818106feea115f
Jung-Jaejin <ymc7737@naver.com>
2018-11-24 12:20:58 +0900

    테스트 시작화면

---
+++ app/src/main/java/com/example/jung_jaejin/myproject/Starttest.java
@@ -0,0 +1,13 @@
+package com.example.jung_jaejin.myproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Starttest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starttest);
+    }
+}
---
+++ app/src/main/java/com/example/jung_jaejin/myproject/Study2.java
@@ -0,0 +1,13 @@
+package com.example.jung_jaejin.myproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Study2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study2);
+    }
+}
