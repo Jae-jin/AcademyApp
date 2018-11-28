03d0a1c8b8ebf61526ea07de0fdc58f5334ea843
Jung-Jaejin <ymc7737@naver.com>
2018-11-28 12:54:13 +0900

    최신

---
+++ app/src/main/java/com/example/jung_jaejin/myproject/Studystart.java
@@ -0,0 +1,13 @@
+package com.example.jung_jaejin.myproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Studystart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_studystart);
+    }
+}
