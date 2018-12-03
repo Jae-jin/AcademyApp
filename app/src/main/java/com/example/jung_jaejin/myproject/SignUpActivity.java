f5b2b838b565fccd8643e35065d0a8f5c0fb9c91
Jung-Jaejin <ymc7737@naver.com>
2018-12-03 13:35:11 +0900

    장호 웰컴 재성이도 곧 웰컴

--- app/src/main/java/com/example/jung_jaejin/myproject/SignUpActivity.java
+++ app/src/main/java/com/example/jung_jaejin/myproject/SignUpActivity.java
@@ -1,13 +1,28 @@
 package com.example.jung_jaejin.myproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
+    Button Next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Next = (Button)findViewById(R.id.next);
+
+        Next.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(),Signup2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
